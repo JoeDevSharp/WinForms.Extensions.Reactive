748c5f6cf20f71477a2d478246d3b7a755d1adb6
José Miguel <clasiccode@gmail.com>
2025-06-21 09:37:27 +0200

    Update ReactiveProperty.cs

--- JoeDevSharp.WinForms.Extensions.Reactive/Core/ReactiveProperty.cs
+++ JoeDevSharp.WinForms.Extensions.Reactive/Core/ReactiveProperty.cs
@@ -1,19 +1,50 @@
 ﻿using System.ComponentModel;
-namespace JoeDevSharp.WinForms.Extensions.Reactive.Core;
-public class ReactiveProperty<T> : INotifyPropertyChanged
+
+namespace JoeDevSharp.WinForms.Extensions.Reactive.Core
 {
-    private T _value;
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public ReactiveProperty(T initialValue = default)
+    /// <summary>
+    /// Represents a reactive property that notifies listeners when its value changes.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    public class ReactiveProperty<T> : INotifyPropertyChanged
     {
-        _value = initialValue;
-    }
+        private T _value;
 
-    public T Value
-    {
-        get => _value;
-        set
+        /// <summary>
+        /// Occurs when the value of the property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReactiveProperty{T}"/> class with an optional initial value.
+        /// </summary>
+        /// <param name="initialValue">The initial value of the property.</param>
+        public ReactiveProperty(T initialValue = default)
+        {
+            _value = initialValue;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the property. Triggers PropertyChanged when the value changes.
+        /// </summary>
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (!Equals(_value, value))
+                {
+                    _value = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the value of the property explicitly. Triggers PropertyChanged if the value is different.
+        /// </summary>
+        /// <param name="value">The new value to assign.</param>
+        public void SetValue(T value)
         {
             if (!Equals(_value, value))
             {
@@ -21,14 +52,11 @@ public class ReactiveProperty<T> : INotifyPropertyChanged
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
             }
         }
+
+        /// <summary>
+        /// Returns the string representation of the underlying value.
+        /// </summary>
+        /// <returns>The result of calling ToString() on the value.</returns>
+        public override string ToString() => _value?.ToString();
     }
-    public void SetValue(T value)
-    {
-        if (!Equals(_value, value))
-        {
-            _value = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
-        }
-    }
-    public override string ToString() => _value?.ToString();
 }
